de5008359ffe49a7caebae350389ec59a86641ec
soukouna94 <soukainaguezour@gmail.com>
2023-04-20 16:19:39 -0700

    Corrected coding style in 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
-int main()
+
+/**
+ * main - Prints a statement
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	char stringToPrint[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(stringToPrint);
-	return 0;
+
+	return (0);
 }
