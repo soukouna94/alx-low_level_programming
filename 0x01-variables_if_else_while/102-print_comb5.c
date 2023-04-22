f16a0e65a253d52b65e936bdb56af6d64ab5b456
soukouna94 <soukainaguezour@gmail.com>
2023-04-22 12:08:49 -0700

    Created 102-print_comb5 and its documentation

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - Software is eating the World.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if ( j != 9 || i != 9)
+			{
+				for (k = 0; k <= 9; k++)
+				{
+					for (l = k; l <= 9; l++)
+					{
+						if (10 * k + l > 10 * i + j)
+						{
+							putchar(48 + i);
+							putchar(48 + j);
+							putchar(' ');
+							putchar(48 + k);
+							putchar(48 + l);
+							if (10 * i + j < 98)
+							{
+								putchar(',');
+								putchar(' ');
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
