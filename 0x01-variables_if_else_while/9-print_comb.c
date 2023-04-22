376cb756bf206cd0ffb000692936673bbe08a8bb
soukouna94 <soukainaguezour@gmail.com>
2023-04-22 04:24:37 -0700

    Created 9-print_comb and its documentation

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints numbers from 0 to 9 with ,.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(48 + i);
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
