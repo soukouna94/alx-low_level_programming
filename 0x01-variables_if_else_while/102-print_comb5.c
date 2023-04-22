8d89f4f328fa98a9f169ea4d73798b78b94f2e93
soukouna94 <soukainaguezour@gmail.com>
2023-04-22 12:23:28 -0700

    Corrected 102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,22 +16,20 @@ int main(void)
 			{
 				for (k = 0; k <= 9; k++)
 				{
-					for (l = 0; l <= 9; l++)
+					l = 0;
+					while (10 * k + l > 10 *i + j)
 					{
-						if (10 * k + l > 10 * i + j)
-						{
-							putchar(48 + i);
-							putchar(48 + j);
-							putchar(' ');
-							putchar(48 + k);
-							putchar(48 + l);
-							if (10 * i + j < 98)
-							{
-								putchar(',');
-								putchar(' ');
-							}
-						}
+						putchar(48 + i);
+						putchar(48 + j);
+						putchar(' ');
+						putchar(48 + k);
+						putchar(48 + l);
 					}
+					if (10 * i + j < 98)
+					{
+						putchar(',');
+						putchar(' ');
+					}	
 				}
 			}
 		}
