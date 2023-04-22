b58cee4ea060ca5639a89d36de22ddadd9e9c1ae
soukouna94 <soukainaguezour@gmail.com>
2023-04-22 11:49:30 -0700

    Created 101-print_comb4 and it documentation

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - The success combination in business is.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{	
+				putchar(48 + i);
+				putchar(48 + j);
+				putchar(48 + k);
+				if (i < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
