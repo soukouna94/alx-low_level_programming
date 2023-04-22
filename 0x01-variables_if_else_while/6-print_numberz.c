3df591c0342fa1fd66c13696e551b408a6ca03cd
soukouna94 <soukainaguezour@gmail.com>
2023-04-21 19:56:58 -0700

    Created 6-print_numberz and its documentation

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-        int i;
-
-        for (i = 0; i <= 9; i++)
-        {
-                putchar(48 + i);
-        }
-        putchar('\n');
-        return (0);
+	int i;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(48 + i);
+	}
+	putchar('\n');
+	return (0);
 }
