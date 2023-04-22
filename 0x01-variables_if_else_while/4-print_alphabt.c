8a9ce0ba3416da99166f969df1d35c850b76eaf3
soukouna94 <soukainaguezour@gmail.com>
2023-04-21 19:40:09 -0700

    Created 3-print_alphabt and its documentation

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - When I was having that alphabet soup, I never thought that it would pay off.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
