5f50898be2cf80606b1ba66a9856fe6103000840
soukouna94 <soukainaguezour@gmail.com>
2023-04-25 16:47:49 -0700

    Created 3-islower and its documentation

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - Entry Point
+ *
+ * Description: Checks whether a character is in lower case
+ * Return: Always 0 (Success)
+ */
+int _islower(int e)
+{
+	return (e >= 'a' && e <= 'z');
+}
