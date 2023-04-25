5e5b6fe0f8cfdbddac8af10543d23f4c1c3c3736
soukouna94 <soukainaguezour@gmail.com>
2023-04-25 16:27:14 -0700

    Created 2-print_alphabet_x10 and its documentation

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry Point
+ *
+ * Description: Print Alphabet 10 times
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char e;
+	int counter;
+
+	counter = 0;
+
+	while (counter < 10)
+	{
+		e = 'a';
+		while (e <= 'z')
+		{
+			_putchar(e);
+			e++;
+		}
+		counter++;
+	}
+	_putchar('\n');
+}
