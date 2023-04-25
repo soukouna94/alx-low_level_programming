00b34db25ffc39b027a3f399092529b4c78fd140
soukouna94 <soukainaguezour@gmail.com>
2023-04-25 16:01:45 -0700

    Created 1-alphabet and its documentation

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Print Alphabet
+ */
+
+void print_alphabet(void)
+{
+	char e = 'a';
+	while (e <= 'z')
+	{
+		_putchar(e);
+		e++;
+	}
+	_putchar('\n');
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H_
 #define MAIN_H
 int _putchar(char c);
+void print_alphabet(void);
 #endif
