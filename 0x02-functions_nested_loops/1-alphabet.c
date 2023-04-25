ecb796901b6e4d8624ec92334cb47ca9dce09d8d
soukouna94 <soukainaguezour@gmail.com>
2023-04-25 16:13:28 -0700

    Corrected 1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,11 @@
 #include "main.h"
 /**
  * main - Entry Point
+ *
  * Description: Print Alphabet
- * Return: Always 0 (Success) 
+ * @parameter: describe parameter
+ *
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
