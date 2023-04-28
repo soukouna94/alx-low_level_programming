3c48fe1611d973511dc970187e22d07602f35750
soukouna94 <soukainaguezour@gmail.com>
2023-04-28 16:39:48 -0700

    Created 1-isdigit and its documentation

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,9 +3,8 @@
 /**
  * _isdigit - checks if it's a digit from 0 to 9
  * @c: character
- * Return: 1 if uppercase, 0 otherwise
+ * Return: 1 if it's a digit from 0  to 9, 0 otherwise
  */
-
 int _isdigit(int c)
 {
 	return ( c >= '0' && c <= '9');
