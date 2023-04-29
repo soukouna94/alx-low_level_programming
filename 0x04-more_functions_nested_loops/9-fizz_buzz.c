e073198c6325f5619ca5e58b0e65bd5fce6ce390
soukouna94 <soukainaguezour@gmail.com>
2023-04-28 19:14:30 -0700

    Corrected 9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,48 +5,34 @@
  * Description: Prints numbers form 1 to 100 and use Buzz and Fizz for //3
  * Return: None
  */
-void printStr(char listToPrint[], int listSize)
-{	int k;
-
-	for (k = 0; k < listSize; k++)
-	{
-		_putchar(listToPrint[k]);
-	}
-}
 int main(void)
 {
 	int i;
-	char fizz[] = " Fizz", buzz[] = " Buzz", fizzbuzz[] = " FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printStr(fizz, 5);
+			printf("Fizz");
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printStr(buzz, 5);
+			printf("Buzz");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printStr(fizzbuzz, 9);
+			printf("FizzBuzz");
 		}
 		else if (i == 1)
 		{
-			_putchar(i + 48);
+			printf("%d", i);
 		}
 		else
 		{
-			_putchar(' ');
-			if ((i / 10) > 0)
-			{
-				_putchar((i / 10) + 48);
-			} 
-			_putchar((i % 10) + 48);
+			printf(" %d", i);
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 
 	return (0);
 }
