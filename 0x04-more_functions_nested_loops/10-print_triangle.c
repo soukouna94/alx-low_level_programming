5207b927cc6de0973490160237a0f44c2dc1ac11
soukouna94 <soukainaguezour@gmail.com>
2023-04-28 19:25:16 -0700

    Added 10-print_triangle and its documentation

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle line in terminal
+ * @size: an integer which represents the size of the triangle
+ * Description: Print a triangle
+ * Return: None
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -8,3 +8,4 @@
 7. I feel like I am diagonally parked in a parallel universe : created a diagonal line on the terminal.
 8. You are so much sunshine in every square inch : created a function which prints a square on the terminal.
 9. Fizz-Buzz : Print numbers from 0 to 10, for multiple of 3, print Fizz x3 and for multiple of 5x print.
+10. Triangles : The program writes a triangle line.
