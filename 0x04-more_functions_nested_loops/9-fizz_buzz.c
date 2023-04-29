9bd7a386732638db7e9b5c3385389194694af304
soukouna94 <soukainaguezour@gmail.com>
2023-04-28 19:10:19 -0700

    Created 9-fizz_buzz and its documentation

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,34 +5,32 @@
  * Description: Prints numbers form 1 to 100 and use Buzz and Fizz for //3
  * Return: None
  */
+void printStr(char listToPrint[], int listSize)
+{	int k;
+
+	for (k = 0; k < listSize; k++)
+	{
+		_putchar(listToPrint[k]);
+	}
+}
 int main(void)
 {
-	int i, k;
-	char fizz[] = "Fizz", buzz[] = "Buzz";
-	char fizzbuzz[] = "FizzBuzz";
+	int i;
+	char fizz[] = " Fizz", buzz[] = " Buzz", fizzbuzz[] = " FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			for (k = 0; k < 4; k++)
-			{
-				_putchar(fizz[k]);
-			}
+			printStr(fizz, 5);
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			for (k = 0; k < 4; k++)
-			{
-				_putchar(buzz[k]);
-			}
+			printStr(buzz, 5);
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			for (k = 0; k < 8; k++)
-			{
-				_putchar(fizzbuzz[k]);
-			}
+			printStr(fizzbuzz, 9);
 		}
 		else if (i == 1)
 		{
@@ -41,7 +39,11 @@ int main(void)
 		else
 		{
 			_putchar(' ');
-			_putchar(i + 48);
+			if ((i / 10) > 0)
+			{
+				_putchar((i / 10) + 48);
+			} 
+			_putchar((i % 10) + 48);
 		}
 	}
 	_putchar('\n');
