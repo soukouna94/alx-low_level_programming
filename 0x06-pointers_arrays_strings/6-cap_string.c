12c88f079e759f756d7e8f1e1be38e02f76e8c3d
soukouna94 <soukainaguezour@gmail.com>
2023-05-04 16:40:27 -0700

    Corrected 6-cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -22,7 +22,7 @@ char *cap_string(char *s)
 		}
 		for (j = 0; j < 13; j++)
 		{
-			if (s[i] != delimiters[j])
+			if (s[i] == delimiters[j])
 			{
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				{
