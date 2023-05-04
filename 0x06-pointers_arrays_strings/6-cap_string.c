7cc47e0e1d7aa124c6906d6a7b39f031108953e0
soukouna94 <soukainaguezour@gmail.com>
2023-05-04 16:27:07 -0700

    Created 6-cap_string and its documentation

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all words of a string
+ * @s: string where each will word will be capitalize
+ * Description: Capitalize all words of a string
+ * Return: Return pointer of the capitalize string
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+
+	char delimiters[13] = {'\n', '\t', '.', ';', ' ', ',',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] != delimiters[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] = s[i + 1] - 32;
+				}
+			}
+		}
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -4,3 +4,4 @@
 3. strcmp : Function which compares two strings
 4. I am a kind of paranoid in reverse. I suspect people of plotting to make me happy : Function reverses the content of an array of integers
 5. Always look up : Function which changes all lowercases of a string into uppercase
+6. Expect the best. Prepare for the worst. Capitalize on what comes : Function all words of a string
