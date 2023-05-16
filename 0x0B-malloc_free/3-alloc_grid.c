0a253a5f56278a5e37a8cc92f991a9914f5293d2
soukouna94 <soukainaguezour@gmail.com>
2023-05-16 16:50:36 -0700

    Created 3-alloc_grid and its documentation

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 	array = (int **)malloc((sizeof(int) * size) + 1);
 	for (h = 0; h < height; h++)
 	{
-		array[h] = (int *)malloc((sizeof(int) + height) + 1);
+		array[h] = (int *)malloc((sizeof(int) + width) + 1);
 		if (array[h] == NULL)
 		{
 			i = h;
@@ -32,9 +32,9 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	for (h = 0; h < height; w++)
+	for (h = 0; h < height; h++)
 	{
-		for (w = 0; w < width; h++)
+		for (w = 0; w < width; w++)
 		{
 			array[h][w] = 0;
 		}
